b94ea91fb348c3ac5d1fcb2561eb0eca3629e845
Chely Ho <adept_inept@yahoo.com>
2018-02-08 20:08:04 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,11 +17,11 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, data|
     if person = "Freddy Mercury"
-     data.each do |key, value|
+      data.each do |key, value|
         if value == :favorite_ice_cream_flavors
           value.delete_if{|ice_cream| ice_cream == "strawberry"}
         end
-     end
+      end
     end
   end
 end
